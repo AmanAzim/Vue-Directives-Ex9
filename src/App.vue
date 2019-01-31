90f0aa9c715d28e56ac159507792dc4a2a2f689e
Aman <rumman.djuice1@gmail.com>
2019-01-31 21:29:32 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@
 
                 <button v-myOn:click="myFunction">Click</button>
                 <h1>{{counter}}</h1>
+                <hr>
+                <button v-myOn2="myFunction2">Click to alert</button>
+                <hr>
+                <button v-myOn3:click="myFunction">Click</button>
+                <h1>{{counter}}</h1>
+
+                <input v-myOn3:input="updateName">
+                <p>Name: {{name}}</p>
+
+                <div style="width: 100px; height: 100px; background-color: lightcoral"
+                     v-custom:mouseenter=" mouseEnter"
+                     v-custom:mouseleave="mouseLeft">
+                </div>
 
             </div>
         </div>
@@ -25,15 +38,53 @@
                     }
                 }
             },
+           'myOn2':{
+               bind(el, binding, vnode){
+                   el.onclick=function () {
+                       binding.value();
+                   }
+               }
+           },
+           'myOn3':{
+               bind(el, binding, vnode){
+                   const Event=binding.arg;
+                   const fun=binding.value;
+
+                   el.addEventListener(Event, fun);
+               }
+           },
+           'custom':{
+               bind(el, binding, vnode){
+                   const Event=binding.arg;
+                   const fun=binding.value;
+
+                   el.addEventListener(Event, fun);
+               }
+           },
         },
         data(){
             return{
                 counter:0,
+                name:'',
             };
         },
         methods:{
             myFunction(){
                 this.counter++;
+            },
+            myFunction2(){
+                alert("Aman");
+            },
+            updateName(event){
+              this.name=event.target.value.toLowerCase();
+            },
+            mouseEnter()
+            {
+                console.log('Mouse entered');
+            },
+            mouseLeft()
+            {
+                console.log('Mouse left');
             }
         }
     }
